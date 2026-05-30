136320a9784a8f4cc8742879927c7448cd9bf17a
RomainSaillet <RomainSaillet@users.noreply.github.com>
2026-05-30 23:34:17 +0200

    fix: remove sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
   site: 'https://romainsaillet.com',
-  integrations: [sitemap()],
 });
